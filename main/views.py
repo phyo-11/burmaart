0e1eb771b4dcb99149eee683101fc7c4020bc429
phyo11 <phyonaing11@gmail.com>
2026-09-12 21:49:59 +0630

    update

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, this is index")
+def home(request):
+    return render(request, "home.html", {})
     
-
+def about(request):
+    return render(request, "about.html", {})

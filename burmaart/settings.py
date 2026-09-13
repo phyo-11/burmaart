92c216d41aac74163f880023b7f47eee8b18845d
phyo11 <phyonaing11@gmail.com>
2026-09-13 20:46:36 +0630

    update

--- burmaart/settings.py
+++ burmaart/settings.py
@@ -119,3 +119,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_ROOT = "BASE_DIR / uploads"
--- main/views.py
+++ main/views.py
@@ -6,3 +6,6 @@ def home(request):
     
 def about(request):
     return render(request, "about.html", {})
+
+def product(request):
+    return render(request, "product.html", {})
